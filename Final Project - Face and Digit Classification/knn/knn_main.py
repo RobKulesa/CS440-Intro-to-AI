cbbfeaada4ccef9b356df8ed8b97dcb4997420bc
RobKulesa <robjkulesa@gmail.com>
2021-12-14 12:26:23 -0500

    upload stats for digits

--- Final Project - Face and Digit Classification/knn/knn_main.py
+++ Final Project - Face and Digit Classification/knn/knn_main.py
@@ -9,7 +9,7 @@ import os
 from knn import KNN
 
 
-def load_labels_file(filename: str):
+def load_labels_file(filename: str) -> List[int]:
     lines = read_lines(filename)
     labels = list()
     for line in lines:
@@ -57,9 +57,9 @@ def translate_pixels(line: str) -> List[int]:
 def main(raw_args=None):
     parser = argparse.ArgumentParser()
     parser.add_argument('dataset', help='choose dataset (face, digits)', type=str)
-    parser.add_argument('-p', '--percent', help='choose percent of training data to use', type=float, default=100.0)
-    parser.add_argument('-k', '--kvals', help='k-vals to test, separated by commas (ex: \'35,20,15, 1\'), leave blank to use optimal k (digits: 1, faces: 5)', type=str, default='')
-    parser.add_argument('-f', '--file', help='append results to file', action='store_true', default=False)
+    parser.add_argument('-p', '--percent', help='choose percent of training data to use (ex: 15.5)', type=float, default=100.0)
+    parser.add_argument('-k', '--kvals', help='k-vals to test, separated by commas (ex: \'35,20,15,1\'), leave blank to use optimal k (digits: 1, faces: 5)', type=str, default='')
+    parser.add_argument('-f', '--file', help='append results to results file', action='store_true', default=False)
     parser.add_argument('-s', '--samples', help='use a random subset of 4 samples', action='store_true', default=False)
     parser.add_argument('-v', '--verbose', help='print verbose print strings', action='store_true', default=False)
 
@@ -68,6 +68,7 @@ def main(raw_args=None):
     training_percent = args.percent
     k_vals_in = args.kvals
 
+    # parse kvals into dict
     if k_vals_in == '':
         if dset == 'digits':
             k_vals_in = [1]
@@ -78,13 +79,12 @@ def main(raw_args=None):
         k_vals_in.sort(reverse=True)
     k_vals = dict.fromkeys(k_vals_in, 0)
 
-    if args.verbose:
-        print('Getting data...')
-
     if args.file:
         filename = 'knn_test_results_' + dset
         file = open(filename, 'a')
 
+    if args.verbose:
+        print('Getting data...')
     if dset == 'digits':
         width = 28
         height = 28
@@ -119,14 +119,14 @@ def main(raw_args=None):
     if args.verbose:
         print('Done!')
 
-    print_counter = 0
     if args.samples:
         plt.figure(1)
 
+    print_counter = 0
+    counter = 0
     if args.verbose:
         print('Testing on %.2f%% of training data' % training_percent)
     start_time = datetime.datetime.now()
-    counter = 0
     for i, test_row in df_test_sample.iterrows():
         predictions = my_knn.knn(test_row[:-1], k_vals)
         for idx, k_val in enumerate(k_vals):
@@ -150,9 +150,11 @@ def main(raw_args=None):
                 print_counter += 1
         counter += 1
     testing_time = (datetime.datetime.now() - start_time).total_seconds()
-    selected_k = max(k_vals, key=k_vals.get)
     if args.verbose:
         print('Done!')
+
+    selected_k = max(k_vals, key=k_vals.get)
+
     if args.file:
         print('%.1f%% [%d training, %d testing]' % (training_percent, df_train_sample.shape[0], df_test_sample.shape[0]), file=file)
         if len(k_vals) > 1:
